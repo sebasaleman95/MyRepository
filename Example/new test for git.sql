da6f01762cfa7042eee8e175fbbeb0844fccdea2
alemansebastian.mz@outlook.com <alemansebastian.mz@outlook.com>
2023-10-24 17:21:02 +0000

    new test for main branch

---
+++ Example/new test for git.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+-- MAGIC %md 
+-- MAGIC new Notebook
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC print("Hello test for git")
+
+-- COMMAND ----------
+
+
